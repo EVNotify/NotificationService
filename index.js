560cfe45d8ea838e869474b997e0e01e81ca1088
GPlay97 <GPlay97@users.noreply.github.com>
2020-08-07 00:01:04 +0200

    Preparation for notification handling

---
+++ index.js
@@ -0,0 +1,52 @@
+const cors = require('cors');
+const express = require('express');
+
+const errors = require('./errors.json');
+const port = process.env.PORT || 3006;
+
+const db = require('@evnotify/utils').db;
+const errorHandler = require('@evnotify/middlewares').errorHandler;
+const notificationRouter = require('./routes/notifications');
+
+process.env.NODE_ENV = process.env.NODE_ENV || 'development';
+
+const app = express();
+
+// Cross-Origin-Resource-Sharing support
+app.use(cors({
+    credentials: true,
+    /**
+     * Handles the origin for CORS request
+     * @param {String} origin the origin
+     * @param {Function} callback callback function
+     */
+    origin: (origin, callback) => {
+        if (!origin || origin === 'null') origin = '*';
+        callback(null, origin);
+    }
+}));
+
+// route parsing
+app.use(express.json());
+app.use(express.urlencoded({
+    extended: false
+}));
+
+// route handling
+app.use('/notifications', notificationRouter);
+
+// unknown route
+app.use((_req, _res, next) => next(errors.UNKNOWN_ROUTE));
+
+// error handling
+app.use(errorHandler);
+
+db.connect().then(() => {
+    app.listen(port, () => console.log(`[HTTP] Server started on port ${port}`));
+    app.emit('server_ready');
+}).catch(() => {
+    console.error('Database connection failed');
+    process.exit(1);
+});
+
+module.exports = app;
